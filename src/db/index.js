e14fd4c0aeab5a0086a708e367494cdb1e918b2c
Allauddin <khanallauddin113@gmail.com>
2025-03-05 00:51:21 +0500

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../contants.js";
+
+const connectDB = async () => {
+  try {
+    const connInst = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(`MongoDB Connected: ${connInst.connection.host}`);
+  } catch (error) {
+    console.log("ConnectDB Error: ", error);
+    process.exit(1);
+  }
+};
+
+
+export default connectDB;
